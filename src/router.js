e35869ad77d22ece87d768261c7c3d25731ef4b2
Artzified <electriccollision@gmail.com>
2025-02-15 20:19:07 +0700

    chore: add routes

--- src/router.js
+++ src/router.js
@@ -1,10 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import ContactsPage from '@/pages/ContactsPage.vue'
+import ProjectsPage from '@/pages/ProjectsPage.vue'
+import NotFound from '@/pages/NotFound.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/contacts', component: ContactsPage },
+  { path: '/projects', component: ProjectsPage },
+  { path: '/404', component: NotFound },
+  { path: '/:pathMatch(.*)*', redirect: '/404' }
 ]
 
 const router = createRouter({
