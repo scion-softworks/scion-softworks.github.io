301507f7827a738ede9509b5f32680183f744147
Artzified <electriccollision@gmail.com>
2025-02-15 20:15:35 +0700

    feat: add projects global

--- src/main.js
+++ src/main.js
@@ -8,11 +8,32 @@ const app = createApp(App);
 app.use(router).mount('#app');
 
 app.config.globalProperties.$orgName = 'Scion Softworks';
+app.config.globalProperties.$projects = [
+	{
+		name: 'Parallelizer',
+		description:
+			'Parallelizer is a lightweight buffer-based parallelism solution with minimal overhead in mind',
+		github: 'https://github.com/scion-softworks/parallelizer',
+		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJM0JCzFHFW2cqimo2V2NChHSijFxu8RNS4Q&s',
+		docs: 'scion-softworks.github.io/parallelizer'
+	},
+	{
+		name: 'Muon',
+		description:
+			'Muon is a simple memory store library typescript implementation designed with ease of use in mind. It features exponential backoff for every operation in Hash Maps, Queues, and Sorted Maps',
+		github: 'https://github.com/scion-softworks/Muon',
+		image: 'https://raw.githubusercontent.com/scion-softworks/Muon/refs/heads/master/public/logo.svg',
+		docs: 'https://github.com/scion-softworks/Muon/blob/master/README.md'
+	}
+]
 
 app.mixin({
 	computed: {
 		orgName() {
 			return this.$orgName;
+		},
+		projects() {
+			return this.$projects
 		}
 	}
 });
