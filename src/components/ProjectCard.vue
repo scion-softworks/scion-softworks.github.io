01a1880af7af043d8d749448f8798901fdbf8e4a
Artzified <electriccollision@gmail.com>
2025-02-15 20:16:37 +0700

    feat: add project card

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,45 @@
+<template>
+	<img
+		:src="image"
+		alt="Project screenshot"
+		class="w-full h-48 object-scale-down"
+	/>
+	<div class="p-6">
+		<h3 class="text-xl font-semibold mb-2">{{ name }}</h3>
+		<p class="text-blue-gray-800 mb-4">
+			{{ description }}
+		</p>
+		<div class="flex gap-3">
+			<a :href="github" class="text-blue-600 hover:text-blue-700">GitHub</a>
+			<a :href="docs" class="text-blue-600 hover:text-blue-700">Docs</a>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ProjectCard',
+	props: {
+		name: {
+			type: String,
+			required: true
+		},
+		description: {
+			type: String,
+			required: true
+		},
+		image: {
+			type: String,
+			required: true
+		},
+		github: {
+			type: String,
+			required: true
+		},
+		docs: {
+			type: String,
+			required: false
+		}
+	}
+};
+</script>
