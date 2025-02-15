40e9a8a53feffa9b78e0573a424d4ae78be345d9
Artzified <electriccollision@gmail.com>
2025-02-15 20:18:44 +0700

    feat: add not found page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,33 @@
+<template>
+	<NavBar />
+
+	<div class="flex flex-col items-center justify-center h-screen">
+		<p class="text-8xl pb-16">¯\_(ツ)_/¯</p>
+		<h1 class="text-4xl font-semibold text-blue-gray-800">404 Not Found</h1>
+		<p class="text-blue-gray-600 pb-4 text-lg">
+			The page you're looking for doesn't exist <i>yet</i>
+		</p>
+
+		<router-link
+			to="/"
+			class="bg-blue-600 text-white text-xl px-8 py-3 rounded-lg hover:bg-blue-700 transition-colors"
+		>
+			Go back
+		</router-link>
+	</div>
+
+	<FooterBar />
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue';
+import FooterBar from '@/components/FooterBar.vue';
+
+export default {
+	name: 'ProjectsPage',
+	components: {
+		NavBar,
+		FooterBar
+	}
+};
+</script>
