0007a6f89b62c33d41eb0cc0a9ec9082062891d2
Artzified <electriccollision@gmail.com>
2025-02-16 19:10:22 +0700

    feat: use dynamic links

--- src/main.js
+++ src/main.js
@@ -16,23 +16,32 @@ app.config.globalProperties.$projects = [
 		name: 'Parallelizer',
 		description:
 			'Parallelizer is a lightweight buffer-based parallelism solution with minimal overhead in mind',
-		github: 'https://github.com/scion-softworks/parallelizer',
+		links: {
+			Github: 'https://github.com/scion-softworks/parallelizer',
+			Docs: 'https://scion-softworks.github.io/parallelizer',
+			Wally: 'https://wally.run/package/artzified/parallelizer'
+		},
 		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSJM0JCzFHFW2cqimo2V2NChHSijFxu8RNS4Q&s',
-		docs: 'https://scion-softworks.github.io/parallelizer'
 	},
 	{
 		name: 'Muon',
 		description:
 			'Muon is a simple memory store library typescript implementation designed with ease of use in mind. It features exponential backoff for every operation in Hash Maps, Queues, and Sorted Maps',
-		github: 'https://github.com/scion-softworks/Muon',
+		links: {
+			Github: 'https://github.com/scion-softworks/Muon',
+			Docs: 'https://github.com/scion-softworks/Muon/blob/master/README.md',
+			NPM: 'https://www.npmjs.com/package/@rbxts/muon'
+		},
 		image: 'https://raw.githubusercontent.com/scion-softworks/Muon/refs/heads/master/public/logo.svg',
-		docs: 'https://github.com/scion-softworks/Muon/blob/master/README.md'
 	},
 	{
 		name: 'PriorityQueue',
 		description:
 			'PriorityQueue is a simple priority queue typescript implementation with both MinHeapPriorityQueue and MaxHeapPriorityQueue. Both of them inherit the same base PriorityQueue class',
-		github: 'https://github.com/scion-softworks/PriorityQueue',
+		links: {
+			Github: 'https://github.com/scion-softworks/PriorityQueue',
+			NPM: 'https://www.npmjs.com/package/@rbxts/priorityqueue'
+		},
 		image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTvPMwSiKxkkg3XpuyWqi_GgixTA5BSKnRj0A&s'
 	}
 ];
