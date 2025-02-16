1867c83d010c4796bbdcccba379faef13908ac4e
Artzified <electriccollision@gmail.com>
2025-02-16 19:09:48 +0700

    feat: dynamic links

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -9,9 +9,15 @@
 		<p class="text-blue-gray-800 mb-4">
 			{{ description }}
 		</p>
-		<div class="flex gap-3">
-			<a :href="github" target="_blank" class="text-blue-600 hover:text-blue-800 hover:underline transition-colors duration-300">GitHub</a>
-			<a :href="docs" v-if="docs" target="_blank" class="text-blue-600 hover:text-blue-800 hover:underline transition-colors duration-300">Docs</a>
+		<div class="flex gap-3" v-if="Object.keys(links).length">
+			<div v-for="(url, key) in links" :key="key">
+				<a
+					:href="url"
+					target="_blank"
+					class="text-blue-600 hover:text-blue-800 hover:underline transition-colors duration-300"
+					>{{ key }}</a
+				>
+			</div>
 		</div>
 	</div>
 </template>
@@ -32,13 +38,10 @@ export default {
 			type: String,
 			required: true
 		},
-		github: {
-			type: String,
-			required: true
-		},
-		docs: {
-			type: String,
-			required: false
+		links: {
+			type: Object,
+			required: false, // Set to false if you want it to be optional
+			default: () => ({}) // Provide an empty object as the default value
 		}
 	}
 };
