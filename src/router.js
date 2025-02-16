419d2ea01817856b8fb6147fcd63c51d5c60a739
Artzified <electriccollision@gmail.com>
2025-02-16 13:54:37 +0700

    chore: add contribute page

--- src/router.js
+++ src/router.js
@@ -2,12 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomePage from '@/pages/HomePage.vue'
 import ContactsPage from '@/pages/ContactsPage.vue'
 import ProjectsPage from '@/pages/ProjectsPage.vue'
+import ContributePage from '@/pages/ContributePage.vue'
+
 import NotFound from '@/pages/NotFound.vue'
 
 const routes = [
   { path: '/', component: HomePage },
   { path: '/contacts', component: ContactsPage },
   { path: '/projects', component: ProjectsPage },
+  { path: '/contribute', component: ContributePage },
   { path: '/404', component: NotFound },
   { path: '/:pathMatch(.*)*', redirect: '/404' }
 ]
