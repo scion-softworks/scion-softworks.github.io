408a19df0e3864db71df0b153b1ea097332c48da
Artzified <electriccollision@gmail.com>
2025-02-21 19:23:39 +0700

    feat: adjust responsive grid columns

--- src/pages/ContributePage.vue
+++ src/pages/ContributePage.vue
@@ -221,7 +221,7 @@
 							</div>
 
 							<div
-								class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4"
+								class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4"
 							>
 								<a
 									v-for="contributor in contributors"
